5055ae4f0a39bc8b9508b9b99e742f7a30ba2378
ranrej <rania.rejdal@ontariotechu.net>
2024-11-14 11:44:20 -0500

    Wallet creating setup

--- MobileProject/lib/WalletPage/wallet-main.dart
+++ MobileProject/lib/WalletPage/wallet-main.dart
@@ -69,8 +69,195 @@ class WalletMain extends StatelessWidget {
                 ],
               ),
             ),
+            // AddAccount Button
+            Positioned(
+              top: 100,
+              left: 16,
+              child: TextButton.icon(
+                onPressed: () {
+                  // TODO: Implement AddAccount functionality
+                },
+                icon: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  size: 45,
+                ),
+                label: const Text(
+                  'ADD Account',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  side: BorderSide(color: Colors.transparent),
+                ),
+              ),
+            ),
+            // List of VaultCards
+            Positioned(
+              top: 160,
+              left: 16,
+              right: 16,
+              child: Column(
+                children: const [
+                  VaultCard(),
+                  SizedBox(height: 16),
+                  VaultCard(),
+                  SizedBox(height: 16),
+                  VaultCard(),
+                ],
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class VaultCard extends StatelessWidget {
+  const VaultCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 343,
+      height: 198,
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [Color(0xFF1A2887), Color(0xFF7E1D9C)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Stack(
+        children: [
+          // Edit Button
+          Positioned(
+            top: 8,
+            right: 8,
+            child: Container(
+              width: 30,
+              height: 30,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.grey, width: 1),
+              ),
+              child: const Icon(
+                Icons.edit_outlined,
+                color: Colors.white,
+                size: 18,
+              ),
+            ),
+          ),
+          // Vault Name
+          Positioned(
+            top: 16,
+            left: 16,
+            child: const Text(
+              'School Trip',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Poppins',
+              ),
+            ),
+          ),
+          // Remaining Days
+          Positioned(
+            top: 40,
+            left: 16,
+            child: const Text(
+              '38 Days Remaining',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 13,
+                fontFamily: 'Poppins',
+              ),
+            ),
+          ),
+          // Vault Image
+          Positioned(
+            bottom: 16,
+            left: 16,
+            child: Container(
+              width: 81,
+              height: 81,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.3),
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: Image(
+                  image: AssetImage('assets/temporary/vault_image.png'),
+                  width: 81,
+                  height: 81,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          // Goal Column
+          Positioned(
+            bottom: 16,
+            left: 120,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Goal',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+                Text(
+                  '\$5000',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Balance Column
+          Positioned(
+            bottom: 16,
+            left: 220,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Balance',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+                Text(
+                  '\$2000',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
