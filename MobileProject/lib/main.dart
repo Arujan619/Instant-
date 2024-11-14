2b4595aed72ca7aff48495c99fb4a7433b9a60cc
ranrej <rania.rejdal@ontariotechu.net>
2024-11-13 20:24:18 -0500

    Setup firebase and firebase plugins

--- MobileProject/lib/main.dart
+++ MobileProject/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'LandingPage/landing-main.dart';
 import 'HomePage/home-main.dart';
 
@@ -17,7 +19,11 @@ import 'HomePage/home-main.dart';
     - If user not logged in, show LandingPage, else show HomePage. For now just show LandingPage
  */
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
