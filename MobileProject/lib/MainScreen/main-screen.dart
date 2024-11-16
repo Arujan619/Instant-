ce3f182eefba9da46dc9a4d88406dc2d9bc71e81
elainey188 <elaine.nankanja@dcmail.ca>
2024-11-15 21:49:24 -0500

    Methods to store profile data in firebase added.

--- MobileProject/lib/MainScreen/main-screen.dart
+++ MobileProject/lib/MainScreen/main-screen.dart
@@ -20,7 +20,7 @@ class _MainScreenState extends State<MainScreen> {
     end: Alignment.bottomRight,
   );
 
-  static const List<Widget> _widgetOptions = <Widget>[
+  static List<Widget> _widgetOptions = <Widget>[
     HomeMain(),
     WalletMain(),
     MetricsMain(),
